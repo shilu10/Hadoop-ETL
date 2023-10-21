9fdd5725bb4299cf2b9412e8968e0ca07b3a0c48
Shilu <shilu842003@gmail.com>
2023-10-21 06:58:58 +0530

    updated the create_table_dag

--- dags/create_table_dag.py
+++ dags/create_table_dag.py
@@ -15,7 +15,7 @@ default_args = { 'owner': 'shilu',
          # If a task fails, retry it once after waiting 
          # at least 5 minutes 
          #'retries': 1,
-         'retry_delay': timedelta(minutes=5),
+         'retry_delay': timedelta(minutes=5),}
 
 
 dag_mysql = DAG(
@@ -29,13 +29,43 @@ dag_mysql = DAG(
 )
 
 
-create_sql_statement = """ CREATE TABLE demo.customer(customer_id VARCHAR(100), 
+customer_createT_sql_statement = """ CREATE TABLE demo.customer(customer_id VARCHAR(100), 
                                     customer_unique_id VARCHAR(100), 
-                                    customer_zip_code_prefix int,
+                                    customer_zip_code_prefix INT,
                                     customer_city CHAR(20), 
                                     customer_state CHAR(20)); """
 
-create_table = MySqlOperator(sql=create_sql_statement, 
-                task_id="CreateTable",
+customer_create_table = MySqlOperator(sql=customer_createT_sql_statement, 
+                task_id="CustomerTableCreation",
                 mysql_conn_id="mysql_conn", 
                 dag=dag_mysql)
+
+
+seller_createT_sql_statement = """ CREATE TABLE demo.seller(seller_id VARCHAR(100), 
+                                  seller_zip_code_prefix INT,
+                                  seller_city CHAR(20),
+                                  seller_state CHAR(20)); """
+
+seller_create_table = MySqlOperator(sql=seller_createT_sql_statement, 
+                task_id="SellerTableCreation",
+                mysql_conn_id="mysql_conn", 
+                dag=dag_mysql)
+
+
+products_createT_sql_statement = """ CREATE TABLE demo.products(product_id VARCHAR(100), 
+                                  product_category_name CHAR(100),
+                                  product_name_length FLOAT,
+                                  product_description_lenght FLOAT, 
+                                  product_photos_qty FLOAT, 
+                                  product_weight_g FLOAT, 
+                                  product_length_cm FLOAT, 
+                                  product_height_cm FLOAT, 
+                                  product_width_cm FLOAT); """
+
+products_create_table = MySqlOperator(sql=products_createT_sql_statement, 
+                task_id="SellerTableCreation",
+                mysql_conn_id="mysql_conn", 
+                dag=dag_mysql)
+
+
+customer_create_table >> seller_create_table >> products_create_table
