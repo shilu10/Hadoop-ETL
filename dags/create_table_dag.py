1b3f14e928c0a3089f62329cbfb1397904d70b66
adminuser <adminuser@kali.mvm>
2023-10-21 07:22:45 +0530

    updated the create_table_dag

--- dags/create_table_dag.py
+++ dags/create_table_dag.py
@@ -1,5 +1,6 @@
-from airflow.decorators import dag
-from datetime import datetime
+import airflow
+from datetime import timedelta
+from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.operators.mysql_operator import MySqlOperator
 from airflow.utils.dates import days_ago
@@ -52,7 +53,7 @@ seller_create_table = MySqlOperator(sql=seller_createT_sql_statement,
                 dag=dag_mysql)
 
 
-products_createT_sql_statement = """ CREATE TABLE demo.products(product_id VARCHAR(100), 
+products_createT_sql_statement = """ CREATE TABLE demo.product(product_id VARCHAR(100), 
                                   product_category_name CHAR(100),
                                   product_name_length FLOAT,
                                   product_description_lenght FLOAT, 
@@ -63,9 +64,51 @@ products_createT_sql_statement = """ CREATE TABLE demo.products(product_id VARCH
                                   product_width_cm FLOAT); """
 
 products_create_table = MySqlOperator(sql=products_createT_sql_statement, 
-                task_id="SellerTableCreation",
+                task_id="ProductsTableCreation",
                 mysql_conn_id="mysql_conn", 
                 dag=dag_mysql)
 
 
-customer_create_table >> seller_create_table >> products_create_table
+geolocation_createT_sql_statement = """ CREATE TABLE demo.geolocation(product_id VARCHAR(100), 
+                                  geolocation_zip_code_prefix INT,
+                                  geolocation_lat FLOAT,
+                                  geolocation_lng FLOAT, 
+                                  geolocation_city CHAR(100), 
+                                  geolocation_state CHAR(100)); """
+
+geolocation_create_table = MySqlOperator(sql=geolocation_createT_sql_statement, 
+                task_id="GeolocationTableCreation",
+                mysql_conn_id="mysql_conn", 
+                dag=dag_mysql)
+
+
+orders_createT_sql_statement = """ CREATE TABLE demo.order(order_id VARCHAR(100), 
+                                  customer_id VARCHAR(100),
+                                  order_status CHAR(100),
+                                  order_purchase_timestamp CHAR(100), 
+                                  order_approved_at CHAR(100), 
+                                  order_delivered_carrier_date CHAR(100), 
+                                  order_delivered_customer_date CHAR(100), 
+                                  order_estimated_delivery_date CHAR(100)); """
+
+orders_create_table = MySqlOperator(sql=orders_createT_sql_statement, 
+                task_id="OrdersTableCreation",
+                mysql_conn_id="mysql_conn", 
+                dag=dag_mysql)
+
+
+orders_item_createT_sql_statement = """ CREATE TABLE demo.order(order_id VARCHAR(100), 
+                                  product_id VARCHAR(100),
+                                  order_item_id INT,
+                                  seller_id VARCHAR(100), 
+                                  shipping_limit_date CHAR(100), 
+                                  price FLOAT, 
+                                  freight_value FLOAT); """
+
+orders_item_create_table = MySqlOperator(sql=orders_item_createT_sql_statement, 
+                task_id="OrdersItemTableCreation",
+                mysql_conn_id="mysql_conn", 
+                dag=dag_mysql)
+
+
+customer_create_table >> seller_create_table >> products_create_table >> geolocation_create_table >> orders_create_table >> orders_item_create_table
