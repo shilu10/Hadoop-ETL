3006c274fbcd7625225eaacd92221a9b8e6c71b9
Shilu <shilu842003@gmail.com>
2023-10-21 08:43:27 +0530

    updated the create_table_dag

--- dags/create_table_dag.py
+++ dags/create_table_dag.py
@@ -30,11 +30,11 @@ dag_mysql = DAG(
 )
 
 
-customer_createT_sql_statement = """ CREATE TABLE demo.customer(customer_id VARCHAR(100), 
-                                    customer_unique_id VARCHAR(100), 
-                                    customer_zip_code_prefix INT,
-                                    customer_city CHAR(20), 
-                                    customer_state CHAR(20)); """
+customer_createT_sql_statement = """ CREATE TABLE demo.customer(customer_id VARCHAR(255) PRIMARY KEY, 
+                                    customer_unique_id VARCHAR(255) NOT NULL, 
+                                    customer_zip_code_prefix INT NOT NULL,
+                                    customer_city CHAR(255) NOT NULL, 
+                                    customer_state CHAR(255) NOT NULL); """
 
 customer_create_table = MySqlOperator(sql=customer_createT_sql_statement, 
                 task_id="CustomerTableCreation",
@@ -42,10 +42,10 @@ customer_create_table = MySqlOperator(sql=customer_createT_sql_statement,
                 dag=dag_mysql)
 
 
-seller_createT_sql_statement = """ CREATE TABLE demo.seller(seller_id VARCHAR(100), 
-                                  seller_zip_code_prefix INT,
-                                  seller_city CHAR(20),
-                                  seller_state CHAR(20)); """
+seller_createT_sql_statement = """ CREATE TABLE demo.seller(seller_id VARCHAR(255) PRIMARY KEY, 
+                                  seller_zip_code_prefix INT NOT NULL,
+                                  seller_city CHAR(255) NOT NULL,
+                                  seller_state CHAR(255) NOT NULL); """
 
 seller_create_table = MySqlOperator(sql=seller_createT_sql_statement, 
                 task_id="SellerTableCreation",
@@ -53,8 +53,8 @@ seller_create_table = MySqlOperator(sql=seller_createT_sql_statement,
                 dag=dag_mysql)
 
 
-products_createT_sql_statement = """ CREATE TABLE demo.product(product_id VARCHAR(100), 
-                                  product_category_name CHAR(100),
+products_createT_sql_statement = """ CREATE TABLE demo.product(product_id VARCHAR(255) PRIMARY KEY, 
+                                  product_category_name CHAR(255) NOT NULL,
                                   product_name_length FLOAT,
                                   product_description_lenght FLOAT, 
                                   product_photos_qty FLOAT, 
@@ -69,12 +69,11 @@ products_create_table = MySqlOperator(sql=products_createT_sql_statement,
                 dag=dag_mysql)
 
 
-geolocation_createT_sql_statement = """ CREATE TABLE demo.geolocation(product_id VARCHAR(100), 
-                                  geolocation_zip_code_prefix INT,
-                                  geolocation_lat FLOAT,
-                                  geolocation_lng FLOAT, 
-                                  geolocation_city CHAR(100), 
-                                  geolocation_state CHAR(100)); """
+geolocation_createT_sql_statement = """ CREATE TABLE demo.geolocation(geolocation_zip_code_prefix INT NOT NULL,
+                                  geolocation_lat FLOAT NOT NULL,
+                                  geolocation_lng FLOAT NOT NULL, 
+                                  geolocation_city CHAR(100) NOT NULL, 
+                                  geolocation_state CHAR(100) NOT NULL); """
 
 geolocation_create_table = MySqlOperator(sql=geolocation_createT_sql_statement, 
                 task_id="GeolocationTableCreation",
@@ -82,14 +81,17 @@ geolocation_create_table = MySqlOperator(sql=geolocation_createT_sql_statement,
                 dag=dag_mysql)
 
 
-orders_createT_sql_statement = """ CREATE TABLE demo.order(order_id VARCHAR(100), 
-                                  customer_id VARCHAR(100),
-                                  order_status CHAR(100),
-                                  order_purchase_timestamp CHAR(100), 
-                                  order_approved_at CHAR(100), 
-                                  order_delivered_carrier_date CHAR(100), 
-                                  order_delivered_customer_date CHAR(100), 
-                                  order_estimated_delivery_date CHAR(100)); """
+orders_createT_sql_statement = """ CREATE TABLE demo.order(order_id VARCHAR(30) PRIMARY KEY, 
+                                  customer_id VARCHAR(100) NOT NULL,
+                                  order_status CHAR(30) NOT NULL,
+                                  order_purchase_timestamp CHAR(30), 
+                                  order_approved_at CHAR(30), 
+                                  order_delivered_carrier_date CHAR(30), 
+                                  order_delivered_customer_date CHAR(30), 
+                                  order_estimated_delivery_date CHAR(30) NOT NULL
+                                  FOREIGN KEY (customer_id)
+                                    REFERENCES customer (customer_id) 
+                                    ON DELETE CASCADE); """
 
 orders_create_table = MySqlOperator(sql=orders_createT_sql_statement, 
                 task_id="OrdersTableCreation",
@@ -97,24 +99,40 @@ orders_create_table = MySqlOperator(sql=orders_createT_sql_statement,
                 dag=dag_mysql)
 
 
-orders_item_createT_sql_statement = """ CREATE TABLE demo.order_item(order_id VARCHAR(100), 
-                                  product_id VARCHAR(100),
-                                  order_item_id INT,
-                                  seller_id VARCHAR(100), 
+orders_item_createT_sql_statement = """ CREATE TABLE demo.order_item(order_id VARCHAR(255) NOT NULL, 
+                                  product_id VARCHAR(255) NOT NULL,
+                                  order_item_id INT NOT NULL,
+                                  seller_id VARCHAR(100) NOT NULL, 
                                   shipping_limit_date CHAR(100), 
                                   price FLOAT, 
-                                  freight_value FLOAT); """
+                                  freight_value FLOAT
+                                  FOREIGN KEY (order_id)
+                                    REFERENCES order(order_id)
+                                    ON DELETE CASCADE
+
+                                  FOREIGN KEY (product_id)
+                                    REFERENCES product(product_id)
+                                    ON DELETE CASCADE 
+
+                                  FOREIGN KEY (seller_id)
+                                    REFERENCES seller(seller_id)
+                                    ON DELETE CASCADE); """
 
 orders_item_create_table = MySqlOperator(sql=orders_item_createT_sql_statement, 
                 task_id="OrdersItemTableCreation",
                 mysql_conn_id="mysql_conn", 
                 dag=dag_mysql)
 
-orders_payment_createT_sql_statement = """ CREATE TABLE demo.order_payment(order_id VARCHAR(100), 
-                                  payment_sequential INT,
-                                  payment_type CHAR(20),
-                                  payment_installments INT, 
-                                  payment_value FLOAT); """
+orders_payment_createT_sql_statement = """ CREATE TABLE demo.order_payment(order_id VARCHAR(100) NOT NULL, 
+                                  payment_sequential INT NOT NULL,
+                                  payment_type CHAR(20) NOT NULL,
+                                  payment_installments INT NOT NULL, 
+                                  payment_value FLOAT NOT NULL
+
+                                  FOREIGN KEY (order_id)
+                                    REFERENCES order(order_id)
+                                    ON DELETE CASCADE); """
+
 
 orders_payment_create_table = MySqlOperator(sql=orders_payment_createT_sql_statement, 
                 task_id="OrdersPaymentTableCreation",
@@ -122,13 +140,17 @@ orders_payment_create_table = MySqlOperator(sql=orders_payment_createT_sql_state
                 dag=dag_mysql)
 
 
-orders_review_createT_sql_statement = """ CREATE TABLE demo.order_review(order_id VARCHAR(100), 
-                                  review_id VARCHAR(100),
+orders_review_createT_sql_statement = """ CREATE TABLE demo.order_review(order_id VARCHAR(100) NOT NULL, 
+                                  review_id VARCHAR(100) NOT NULL,
                                   review_score INT,
                                   review_comment_title CHAR(10), 
                                   review_comment_message VARCHAR(100), 
                                   review_creation_date VARCHAR(100), 
-                                  review_answer_timestamp VARCHAR(100)); """
+                                  review_answer_timestamp VARCHAR(100)
+
+                                  FOREIGN KEY (order_id)
+                                    REFERENCES order(order_id)
+                                    ON DELETE CASCADE); """
 
 orders_review_create_table = MySqlOperator(sql=orders_review_createT_sql_statement, 
                 task_id="OrdersRerviewTableCreation",
