6870c2b8d523a17babcfe5479bb7e675f9d39f9c
Shilu <shilu842003@gmail.com>
2023-10-23 20:37:58 +0530

    updated the insert_to_db and create_table dags

--- dags/insert_into_db.py
+++ dags/insert_into_db.py
@@ -14,16 +14,6 @@ import numpy as np
 import os 
 
 
-
-
-
-CONN = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
-                              username=os.environ['MYSQL_USERNAME'], 
-                              password=os.environ['MYSQL_PASSWORD']
-                            )
-
-CURSOR = CONN.cursor()
-
 default_args = { 'owner': 'shilu',
          #'start_date': airflow.utils.dates.days_ago(2), 
          # 'end_date': datetime(),
@@ -56,31 +46,44 @@ def customer_table_insert_statement():
     count = 0 
     for indx, row in df.iterrows():
         try:
+            connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+            cursor = connection.cursor()
+
             customer_id = row['customer_id']
             customer_unique_id = row['customer_unique_id']
             customer_zip_code_prefix = row['customer_zip_code_prefix']
             customer_city = row['customer_city']
             customer_state = row['customer_state']
 
+            last_value_statement = "SELECT id from hdfs_project_data.customer ORDER BY id DESC LIMIT 1"
+            cursor.execute(sql_check_statement, last_value_statement)
+            last_value = cursor.fetchone()[0]
+            id = last_value + 1 if last_value else 0
+
             # Check if the record already exists
             sql_check_statement = "SELECT * FROM hdfs_project_data.customer WHERE customer_id = %s"
-            CURSOR.execute(sql_check_statement, (customer_id,))
+            cursor.execute(sql_check_statement, (customer_id,))
 
-            if CURSOR.fetchone():
+            if cursor.fetchone():
                 print("record already exists")
 
             else:
 
-                sql_insert_statement = "INSERT INTO hdfs_project_data.customer (customer_id, customer_unique_id, \
-                        customer_zip_code_prefix, customer_city, customer_state) VALUES (%s, %s, %s, %s, %s)"
+                sql_insert_statement = "INSERT INTO hdfs_project_data.customer (id, customer_id, customer_unique_id, \
+                        customer_zip_code_prefix, customer_city, customer_state) VALUES (%s, %s, %s, %s, %s, %s)"
 
 
                 if count < 200:
-                    CURSOR.execute(sql_insert_statement, 
-                                    (customer_id, customer_unique_id, 
+                    cursor.execute(sql_insert_statement, 
+                                    (id, customer_id, customer_unique_id, 
                                         customer_zip_code_prefix, customer_city, customer_state))
-                    CONN.commit()
+                    connection.commit()
                     count += 1 
+                    connection.close()
 
 
                 else:
@@ -102,30 +105,43 @@ def seller_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor()
+        
         seller_id = row['seller_id']
         seller_zip_code_prefix = row['seller_zip_code_prefix']
         seller_city = row['seller_city']
         seller_state = row['seller_state']
 
+        last_value_statement = "SELECT id from hdfs_project_data.seller ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
+
         # Check if the record already exists
         sql_check_statement = "SELECT * FROM hdfs_project_data.seller WHERE seller_id = %s"
-        CURSOR.execute(sql_check_statement, (seller_id,))
+        cursor.execute(sql_check_statement, (seller_id,))
 
-        if CURSOR.fetchone():
+        if cursor.fetchone():
             print("record already exists")
 
         else:
 
-            sql_insert_statement = "INSERT INTO hdfs_project_data.seller (seller_id, seller_zip_code_prefix, \
-                    seller_city, seller_state) VALUES (%s, %s, %s, %s)"
+            sql_insert_statement = "INSERT INTO hdfs_project_data.seller (id, seller_id, seller_zip_code_prefix, \
+                    seller_city, seller_state) VALUES (%s, %s, %s, %s, %s)"
 
 
             if count < 200:
-                CURSOR.execute(sql_insert_statement, 
-                                (seller_id, seller_zip_code_prefix, 
+                cursor.execute(sql_insert_statement, 
+                                (id, seller_id, seller_zip_code_prefix, 
                                     seller_city, seller_state))
-                CONN.commit()
+                connection.commit()
                 count += 1 
+                connection.close()
 
 
             else:
@@ -144,24 +160,35 @@ def geolocation_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor()
+
         geolocation_zip_code_prefix = row['geolocation_zip_code_prefix']
         geolocation_lat = row['geolocation_lat']
         geolocation_lng = row['geolocation_lng']
         geolocation_city = row['geolocation_city']
         geolocation_state = row['geolocation_state']
 
+        last_value_statement = "SELECT id from hdfs_project_data.geolocation ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
 
-
-        sql_insert_statement = "INSERT INTO hdfs_project_data.geolocation (geolocation_zip_code_prefix, geolocation_lat, \
-                geolocation_lng, geolocation_city, geolocation_state) VALUES (%s, %s, %s, %s, %s)"
+        sql_insert_statement = "INSERT INTO hdfs_project_data.geolocation (id, geolocation_zip_code_prefix, geolocation_lat, \
+                geolocation_lng, geolocation_city, geolocation_state) VALUES (%s, %s, %s, %s, %s, %s)"
 
 
         if count < 200:
-            CURSOR.execute(sql_insert_statement, 
-                            (geolocation_zip_code_prefix, geolocation_lat, 
+            cursor.execute(sql_insert_statement, 
+                            (id, geolocation_zip_code_prefix, geolocation_lat, 
                                 geolocation_lng, geolocation_city, geolocation_state))
-            CONN.commit()
+            connection.commit()
             count += 1 
+            connection.close()
 
 
         else:
@@ -180,6 +207,13 @@ def product_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor()
+
         product_id = row['product_id']
         product_category_name = row['product_category_name']
         product_name_length = row['product_name_lenght']
@@ -190,27 +224,33 @@ def product_table_insert_statement():
         product_height_cm = row['product_height_cm']
         product_width_cm = row['product_width_cm']
 
+        last_value_statement = "SELECT id from hdfs_project_data.product ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
+
         # Check if the record already exists
         sql_check_statement = "SELECT * FROM hdfs_project_data.product WHERE product_id = %s"
-        CURSOR.execute(sql_check_statement, (product_id,))
+        cursor.execute(sql_check_statement, (product_id,))
 
-        if CURSOR.fetchone():
+        if cursor.fetchone():
             print("record already exists")
 
         else:
-            sql_insert_statement = "INSERT INTO hdfs_project_data.product (product_id, product_category_name, \
+            sql_insert_statement = "INSERT INTO hdfs_project_data.product (id, product_id, product_category_name, \
                     product_name_length, product_description_lenght, product_photos_qty, \
                     product_weight_g, product_length_cm, product_height_cm, product_width_cm) \
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
 
             if count < 200:
-                CURSOR.execute(sql_insert_statement, 
-                                (product_id, product_category_name, product_name_length, 
+                cursor.execute(sql_insert_statement, 
+                                (id, product_id, product_category_name, product_name_length, 
                                     product_description_lenght, product_photos_qty, product_weight_g, 
                                     product_length_cm, product_height_cm, product_width_cm))
-                CONN.commit()
+                connection.commit()
                 count += 1 
+                connection.close()
 
 
             else:
@@ -229,6 +269,13 @@ def order_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor()
+
         order_id = row['order_id']
         customer_id = row['customer_id']
         order_status = row['order_status']
@@ -238,27 +285,33 @@ def order_table_insert_statement():
         order_delivered_customer_date = row['order_delivered_customer_date']
         order_estimated_delivery_date = row['order_estimated_delivery_date']
 
+        last_value_statement = "SELECT id from hdfs_project_data.orders ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
+
         # Check if the record already exists
         sql_check_statement = "SELECT * FROM hdfs_project_data.orders WHERE order_id = %s"
-        CURSOR.execute(sql_check_statement, (order_id,))
+        cursor.execute(sql_check_statement, (order_id,))
 
-        if CURSOR.fetchone():
+        if cursor.fetchone():
             print("record already exists")
 
         else:
-            sql_insert_statement = "INSERT INTO hdfs_project_data.orders (order_id, customer_id, \
+            sql_insert_statement = "INSERT INTO hdfs_project_data.orders (id, order_id, customer_id, \
                     order_status, order_purchase_timestamp, order_approved_at, \
                     order_delivered_carrier_date, order_delivered_customer_date, order_estimated_delivery_date) \
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
 
             if count < 200:
-                CURSOR.execute(sql_insert_statement, 
-                                (order_id, customer_id, order_status, 
+                cursor.execute(sql_insert_statement, 
+                                (id, order_id, customer_id, order_status, 
                                     order_purchase_timestamp, order_approved_at, order_delivered_carrier_date, 
                                     order_delivered_customer_date, order_estimated_delivery_date))
-                CONN.commit()
+                connection.commit()
                 count += 1 
+                connection.close()
 
 
             else:
@@ -277,6 +330,13 @@ def order_item_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor()
+
         order_id = row['order_id']
         product_id = row['product_id']
         order_item_id = row['order_item_id']
@@ -285,17 +345,23 @@ def order_item_table_insert_statement():
         price = row['price']
         freight_value = row['freight_value']
 
-        sql_insert_statement = "INSERT INTO hdfs_project_data.order_item (order_id, product_id, \
+        last_value_statement = "SELECT id from hdfs_project_data.order_item ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
+
+        sql_insert_statement = "INSERT INTO hdfs_project_data.order_item (id, order_id, product_id, \
                 order_item_id, seller_id, shipping_limit_date, price, freight_value) \
-                VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
 
 
         if count < 200:
-            CURSOR.execute(sql_insert_statement, 
-                            (order_id, product_id, order_item_id, 
+            cursor.execute(sql_insert_statement, 
+                            (id, order_id, product_id, order_item_id, 
                                 seller_id, shipping_limit_date, price, freight_value))
-            CONN.commit()
+            connection.commit()
             count += 1 
+            connection.close()
 
 
         else:
@@ -314,6 +380,13 @@ def order_review_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor()
+        
         order_id = row['order_id']
         review_id = row['review_id']
         review_score = row['review_score']
@@ -322,28 +395,34 @@ def order_review_table_insert_statement():
         review_creation_date = row['review_creation_date']
         review_answer_timestamp = row['review_answer_timestamp']
 
+        last_value_statement = "SELECT id from hdfs_project_data.order_review ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
+
         # Check if the record already exists
         sql_check_statement = "SELECT * FROM hdfs_project_data.order_review WHERE review_id = %s"
-        CURSOR.execute(sql_check_statement, (review_id,))
+        cursor.execute(sql_check_statement, (review_id,))
 
-        if CURSOR.fetchone():
+        if cursor.fetchone():
             print("record already exists")
 
         else:
 
-            sql_insert_statement = "INSERT INTO hdfs_project_data.order_review (order_id, review_id, \
+            sql_insert_statement = "INSERT INTO hdfs_project_data.order_review (id, order_id, review_id, \
                     review_score, review_comment_title, review_comment_message, \
                     review_creation_date, review_answer_timestamp) \
-                    VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
 
 
             if count < 200:
-                CURSOR.execute(sql_insert_statement, 
+                cursor.execute(sql_insert_statement, 
                                 (order_id, review_id, review_score, review_comment_title,
                                     review_comment_message, review_creation_date, review_answer_timestamp))
-                CONN.commit()
+                
+                connection.commit()
                 count += 1 
-
+                connection.close()
 
             else:
                 return
@@ -361,30 +440,45 @@ def order_payment_table_insert_statement():
 
     count = 0 
     for indx, row in df.iterrows():
+        connection = mysql.connector.connect(host=os.environ['MYSQL_HOST'], 
+                              username=os.environ['MYSQL_USERNAME'], 
+                              password=os.environ['MYSQL_PASSWORD']
+                            )
+
+        cursor = connection.cursor() 
+
         order_id = row['order_id']
         payment_sequential = row['payment_sequential']
         payment_type = row['payment_type']
         payment_installments = row['payment_installments']
         payment_value = row['payment_value']
 
+        last_value_statement = "SELECT id from hdfs_project_data.order_payment ORDER BY id DESC LIMIT 1"
+        cursor.execute(sql_check_statement, last_value_statement)
+        last_value = cursor.fetchone()[0]
+        id = last_value + 1 if last_value else 0
+
         # Check if the record already exists
         sql_check_statement = "SELECT * FROM hdfs_project_data.order_payment WHERE payment_sequential = %s"
-        CURSOR.execute(sql_check_statement, (payment_sequential,))
+        cursor.execute(sql_check_statement, (payment_sequential,))
 
-        if CURSOR.fetchone():
+        if cursor.fetchone():
             print("record already exists")
 
         else:
-            sql_insert_statement = "INSERT INTO hdfs_project_data.order_payment (order_id, payment_sequential, \
-                    review_score, payment_installments, payment_value, payment_type) \
-                    VALUES (%s, %s, %s, %s, %s)"
+            sql_insert_statement = "INSERT INTO hdfs_project_data.order_payment (id, order_id, payment_sequential, \
+                    payment_type, payment_installments, payment_value) \
+                    VALUES (%s, %s, %s, %s, %s, %s)"
 
 
             if count < 200:
-                CURSOR.execute(sql_insert_statement, 
-                                (order_id, payment_sequential, payment_type, payment_installments, payment_value))
-                CONN.commit()
+                cursor.execute(sql_insert_statement, 
+                                (id, order_id, payment_sequential, 
+                                    payment_type, payment_installments, payment_value))
+                
+                connection.commit()
                 count += 1 
+                connection.close()
 
 
             else:
