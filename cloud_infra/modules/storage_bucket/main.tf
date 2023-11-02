69123d5daecbc0fce24cbb76cba0d637f2f45a94
adminuser <adminuser@kali.mvm>
2023-11-02 08:07:45 +0530

    added new cloud infra

--- cloud_infra/modules/storage_bucket/main.tf
+++ cloud_infra/modules/storage_bucket/main.tf
@@ -5,3 +5,23 @@ resource "google_storage_bucket" "this" {
 
  uniform_bucket_level_access = true
 }
+
+
+resource "google_storage_bucket_object" "data_directory" {
+  name          = "source_data/"
+  content       = "Directory contains a source data"
+  bucket        = google_storage_bucket.storage_bucket.this"
+}
+
+resource "google_storage_bucket_object" "extra_file_directory" {
+  name          = "python_files/"
+  content       = "Directory contains a python files"
+  bucket        = google_storage_bucket.storage_bucket.this"
+}
+
+resource "google_storage_bucket_object" "extra_file_directory" {
+  foreach = fileset(path.module, "source_data/*")
+  name          = "python_files/"
+  content       = "Directory contains a python files"
+  bucket        = google_storage_bucket.storage_bucket.this"
+}
